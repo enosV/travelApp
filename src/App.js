4a55043c9e7e8c49b6eed28e8382fa9f858fef11
enosv <enosvazquez@gmail.com>
2022-11-16 22:40:58 -0800

    added components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { CssBaseline } from '@material-ui/core';
+
+import Header from './components/Header/Header'
+import List from './components/List/List'
+import Map from './components/Map/Map'
+
+const App = () => {
+    return (
+        <div>
+            <h1>hello, World!</h1>
+        </div>
+    );
+}
+
+export default App;
